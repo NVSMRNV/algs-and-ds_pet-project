6fe37f06c9bc00394d0dfc7271e1fee7b60ffbe4
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-04-23 19:55:17 +0300

    class 'Array' - updated: 2 fields are added + 4 methods are implemented
    The following fields are added:
    	size_ - array size
    	array_ - pointer to the array
    
    The following methods are implemented:
    	Array() - default constructor;
    	Array(int size) - constructor with parameters;
    	Array(const Array& array) - copy constructor;
    	~Array() - destructor.

--- alg-and-ds_cpp/array/array.cpp
+++ alg-and-ds_cpp/array/array.cpp
@@ -1,14 +1,31 @@
 #include "array.h"
 
+// default constructor
 Array::Array(){
-
+	size_ = 1;
+	array_ = new int[size_];
+	array_[0] = 0;
 }
+
+// constructor with parameters
 Array::Array(int size){
-
+	size_ = (size > 0 ? size : 1);
+	array_ = new int[size_];
+	for (int i = 0; i < size_; i++) {
+		array_[i] = 0;
+	}
 }
+
+// copy constructor
 Array::Array(const Array& array){
-
+	size_ = array.size_;
+	array_ = new int[size_];
+	for (int i = 0; i < size_; i++) {
+		array_[i] = array.array_[i];
+	}
 }
-Array::~Array(){
 
+// destructor
+Array::~Array(){
+	delete[] array_;
 }
--- alg-and-ds_cpp/array/array.h
+++ alg-and-ds_cpp/array/array.h
@@ -1,6 +1,9 @@
 #pragma once
 
 class Array {
+private:
+	int size_;
+	int* array_;
 public:
 	Array();
 	Array(int size);
