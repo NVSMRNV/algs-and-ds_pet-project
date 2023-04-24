4a3c4d35379a760d1533948619e8c4cf4bd5601b
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-04-24 15:56:28 +0300

    class 'Node' + class 'SinglyLinkedList' - created
    The following methods are implemented in the 'Node':
    	Node() - default constructor;
    	Node(int data) - constructor with parameters.
    
    The following methods are implemented in the 'SinglyLinkedList':
    	SinglyLinkedList() - default constructor;
    	~SinglyLinkedList() - destructor.

--- alg-and-ds_cpp/singly-linked-list/sll.cpp
+++ alg-and-ds_cpp/singly-linked-list/sll.cpp
@@ -1,1 +1,20 @@
 #include "sll.h"
+
+Node::Node() {
+	data_ = NULL;
+	next_ = nullptr;
+}
+
+Node::Node(int data) {
+	data_ = data;
+	next_ = nullptr;
+}
+
+SinglyLinkedList::SinglyLinkedList() {
+	head_ = nullptr;
+}
+
+SinglyLinkedList::~SinglyLinkedList() {
+	std::cout << "\n~SinglyLinkedList\n";
+	delete head_;
+}
--- alg-and-ds_cpp/singly-linked-list/sll.h
+++ alg-and-ds_cpp/singly-linked-list/sll.h
@@ -1,1 +1,18 @@
 #pragma once
+#include <iostream>
+
+class Node {
+	int data_;
+	Node* next_;
+	friend class SinglyLinkedList;
+public:
+	Node();
+	Node(int data);
+};
+
+class SinglyLinkedList {
+	Node* head_;
+public:
+	SinglyLinkedList();
+	~SinglyLinkedList();
+};
