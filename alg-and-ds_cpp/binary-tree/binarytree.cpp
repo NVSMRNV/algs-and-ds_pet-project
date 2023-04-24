f21dfa28cdc898013012c2bf6d3b1de81138615d
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-04-24 19:33:17 +0300

    class 'BinaryTree' - created: 2 methods are implemented
    The following methods are implemented:
    	BinaryTree() - default constructor
    	~BinaryTree() - destructor

--- alg-and-ds_cpp/binary-tree/binarytree.cpp
+++ alg-and-ds_cpp/binary-tree/binarytree.cpp
@@ -1,13 +1,25 @@
 #include "binarytree.h"
 
+// default constructor for Node
 Node::Node() {
 	data_ = NULL;
 	left_ = nullptr;
 	right_ = nullptr;
 }
 
+// constructor with parameters for Node
 Node::Node(int data) {
 	data_ = data;
 	left_ = nullptr;
 	right_ = nullptr;
+}
+
+// default constructor for BinaryTree
+BinaryTree::BinaryTree() {
+	root_ = nullptr;
+}
+
+// destructor for BinaryTree
+BinaryTree::~BinaryTree() {
+	// todo
 }
--- alg-and-ds_cpp/binary-tree/binarytree.h
+++ alg-and-ds_cpp/binary-tree/binarytree.h
@@ -10,4 +10,9 @@ public:
 	Node(int data);
 };
 
-
+class BinaryTree {
+	Node* root_;
+public:
+	BinaryTree();
+	~BinaryTree();
+};
