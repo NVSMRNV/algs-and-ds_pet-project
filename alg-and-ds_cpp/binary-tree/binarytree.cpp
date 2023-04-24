ad5e4967cac1a25794c861c23f93cc5f8f1e5339
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-04-24 19:23:04 +0300

    class 'Node' - created: 2 methods are implemented
    The following methods are declared:
    	Node() - default constructor;
    	Node(int data) - constructor with parameters;

--- alg-and-ds_cpp/binary-tree/binarytree.cpp
+++ alg-and-ds_cpp/binary-tree/binarytree.cpp
@@ -1,1 +1,13 @@
 #include "binarytree.h"
+
+Node::Node() {
+	data_ = NULL;
+	left_ = nullptr;
+	right_ = nullptr;
+}
+
+Node::Node(int data) {
+	data_ = data;
+	left_ = nullptr;
+	right_ = nullptr;
+}
--- alg-and-ds_cpp/binary-tree/binarytree.h
+++ alg-and-ds_cpp/binary-tree/binarytree.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <iostream>
+
+class Node {
+	int data_;
+	Node* left_;
+	Node* right_;
+public:
+	Node();
+	Node(int data);
+};
+
+
