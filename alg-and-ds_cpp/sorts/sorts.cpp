4b68ec8ea7697e69070a3a2fc17068fa509260ba
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-05-02 19:26:18 +0300

    'sorts' - updated: 5 sorts + 1 auxiliary method are implemented
    The following methods are implemented:
    	swap(int& a, int& b) - swaps 2 elements;
    	stupidSort(int* a, int n) - stupid sorting;
    	bubbleSort(int* a, int n) - bubble sorting;
    	shakerSort(int* a, int n) - shaker sorting;
    	oddEvenSort(int* a, int n) - odd-even sorting;
    	combSort(int* a, int n) - comb sorting.

--- alg-and-ds_cpp/sorts/sorts.cpp
+++ alg-and-ds_cpp/sorts/sorts.cpp
@@ -1,1 +1,100 @@
 #include "sorts.h"
+
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b; b = tmp;
+}
+
+void stupidSort(int* a, int n) {
+	int i = 0;
+	while (i < n - 1) {
+		if (a[i] > a[i + 1]) {
+			swap(a[i], a[i + 1]);
+			i = 0;
+		}
+		else {
+			i++;
+		}
+	}
+}
+
+void bubbleSort(int* a, int n){
+	int i = 0;
+	while (i < n) {
+		int j = 0;
+		while (j < n - i - 1) {
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+void shakerSort(int* a, int n){
+	int left, right;
+	left = 0; right = n - 1;
+
+	while (left < right) {
+		int i = left;
+		while (i < right) {
+			if (a[i] > a[i + 1]) {
+				swap(a[i], a[i + 1]);
+			}
+			i++;
+		}
+		right--;
+
+		int j = right;
+		while (j > left) {
+			if (a[j] < a[j - 1]) {
+				swap(a[j], a[j - 1]);
+			}
+			j--;
+		}
+		left++;
+	}
+}
+
+void oddEvenSort(int* a, int n){
+	bool is_sorted = false;
+	while (!is_sorted) {
+		is_sorted = true;
+
+		int i = 1;
+		while (i < n - 1) {
+			if (a[i] > a[i + 1]) {
+				swap(a[i], a[i + 1]);
+				is_sorted = false;
+			}
+			i++;
+		}
+
+		int j = 0;
+		while (j < n - 1) {
+			if (a[j] > a[j + 1]) {
+				swap(a[j], a[j + 1]);
+				is_sorted = false;
+			}
+			j++;
+		}
+	}
+}
+
+void combSort(int* a, int n){
+	double factor = 1.247;
+	int step = n / factor;
+
+	while (step >= 1) {
+		int i, j;
+		i = 0;  j = step;
+		while (j < n) {
+			if (a[i] > a[j]) {
+				swap(a[i], a[j]);
+			}
+			i++; j++;
+		}
+		step /= factor;
+	}
+}
--- alg-and-ds_cpp/sorts/sorts.h
+++ alg-and-ds_cpp/sorts/sorts.h
@@ -1,1 +1,9 @@
 #pragma once
+#include <iostream>
+
+void swap(int& a, int& b);
+void stupidSort(int* a, int n);
+void bubbleSort(int* a, int n);
+void shakerSort(int* a, int n);
+void oddEvenSort(int* a, int n);
+void combSort(int* a, int n);
