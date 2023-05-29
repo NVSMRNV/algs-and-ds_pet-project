238c82fab5b664cf6dda4fe16d47342debcc14fe
Nikita Smirnov <nvsmirnov00@yandex.ru>
2023-05-29 19:57:12 +0300

    'cstrings' - updated: 2 methods are implemented
    The following methods are implemented:
    	swap_() - swaps two characters;
    	strreverse_() - reverses the string.

--- alg-and-ds_cpp/cstrings/cstrings.cpp
+++ alg-and-ds_cpp/cstrings/cstrings.cpp
@@ -14,3 +14,16 @@ int strcmp_(const char* str_1, const char* str_2) {
 	} 
 	return *str_1 - *str_2;
 }
+
+void swap_(char& char_1, char& char_2) {
+	char tmp = char_1;
+	char_1 = char_2;
+	char_2 = tmp;
+}
+
+void strrverse_(char* str) {
+	int len = strlen_(str);
+	for(int i = 0, j = len - 1; i < j; i++, j--){
+		swap_(str[i], str[j]);
+	}
+}
--- alg-and-ds_cpp/cstrings/cstrings.h
+++ alg-and-ds_cpp/cstrings/cstrings.h
@@ -2,3 +2,5 @@
 
 int strlen_(const char* str);
 int strcmp_(const char* str_1, const char* str_2);
+void swap_(char& char_1, char& char_2);
+void strrverse_(char* str);
